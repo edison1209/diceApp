aec7c6b46fe747bf238af62e43c12f17c7ca0d62
Mahesh Chandra <maheshpaneru22@gmail.com>
2020-12-06 18:59:07 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:flutter/widgets.dart';
 
 void main() {
   return runApp(
@@ -15,9 +18,72 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  rollLeft();
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                onPressed: () {
+                  rollRight();
+                },
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 40.0,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            rollDice();
+          },
+          child: Text(
+            'ROLL THE DICE',
+            style: TextStyle(color: Colors.white, fontSize: 16.0),
+          ),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.grey),
+              padding: MaterialStateProperty.all(EdgeInsets.all(20.0))),
+        ),
+      ],
+    );
+  }
+
+  void rollDice() {
+    rollLeft();
+    rollRight();
+  }
+
+  void rollLeft() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  void rollRight() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
